03627008c3da622f3f5282199fbd1583e79e30ff
w-artisan <mailtome@i.ua>
2019-10-12 23:21:35 +0300

    update: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  //===Column multiplication
+  //=========================
+  // Second string in multiplication will be always shorter
+  if (first.length < second.length) {
+    [first, second] = [second, first];
+  }
+
+  const num2 = second.split('').reverse();
+  const num1 = first.split('').reverse();
+  const result = [];
+
+  for (let i = 0; i < num2.length; i++) {
+    for (let m = 0; m < num1.length; m++) {
+      let index = i + m; // Get the current array position.
+      result[index] =
+        num2[i] * num1[m] + (index >= result.length ? 0 : result[index]);
+
+      if (result[index] > 9) {
+        // Carrying
+        result[index + 1] =
+          Math.floor(result[index] / 10) +
+          (index + 1 >= result.length ? 0 : result[index + 1]);
+        result[index] %= 10;
+      }
+    }
+  }
+  return result.reverse().join('');
+};
